c6c56cd161e972950a4c64c3ca8c8f76f2a7b10e
programer1983 <programer972@gmail.com>
2022-11-19 11:18:20 +0200

    creating separate template components for styles, buttons and lines

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react"
 import './App.css';
-import PostsList from "./components/PostsList";
+import MyButton from "./components/MyButton";
+import MyInput from "./components/MyInput";
 
 function App() {
-  const [posts, setPosts] = React.useState([
-    {id: 1, title: "React", body: "react library"},
-    {id: 2, title: "React2", body: "react library2"},
-    {id: 3, title: "React3", body: "react library3"},
-  ])
 
 
   return (
     <div className="App">
-      <PostsList posts={posts} title={"Posts List"}/>
+        <MyInput type="rexr" placeholder="Enter your text" />
+        <MyButton>Add New Post</MyButton>
     </div>
   );
 }
---
+++ src/components/MyInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MtInput = (props) => {
+  return (
+    <input {...props} className="my__input"/>
+  )
+}
+
+export default MtInput 
