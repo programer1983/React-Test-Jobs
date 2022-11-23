94beff6584f2a2a69edda3b71646567be0739e83
programer1983 <programer972@gmail.com>
2022-11-23 09:37:47 +0200

    search posts

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import React from "react"
 import './App.css'
-import Select from "./components/Select"
 
 function App() {
-  const [postsSort, setPostsSort] = React.useState("")
   const [posts, setPosts] = React.useState([
-    {id: 1, title: "aa", body: "aa"},
-    {id: 2, title: "гг", body: "гг"},
-    {id: 3, title: "bb", body: "bb"},
+    {id: 1, title: "Ract", body: "React"},
+    {id: 2, title: "Redux", body: "Redux"},
+    {id: 3, title: "Next", body: "Next"},
+    {id: 4, title: "Django", body: "Django"},
+    {id: 5, title: "Ruby", body: "Ruby"},
+    {id: 6, title: "Java", body: "Java"},
   ])
 
-  const sprtedPosts = (sort) => {
-    setPostsSort(sort)
-    setPosts([...posts.sort((a,b) => a[sort].localeCompare(b[sort]))])
-  }
+  const [searchPost, setSearchPost] = React.useState("")
 
+  const searchedPosts = () => {
+    return posts.filter(post => post.title.toLowerCase().includes(searchPost.toLowerCase()))
+  }
+ 
+  const searchpost = searchedPosts()
 
   return (
     <div className="App">
-      <div className="select">
-        <Select
-          value={postsSort}
-          sprtedPosts={sprtedPosts}
-          defaultname="Select"
-          options={[
-            {value: "title", name: "By title"},
-            {value: "body", name: "By description"},
-          ]}
-        />
-      </div>
+      <input 
+         value={searchPost}
+         type="text" 
+         onChange={e => setSearchPost(e.target.value)}
+         placeholder="Search"
+      />
+
       
       <div className="posts__list">
-        {posts.map((post) => (
+        {searchpost.map((post) => (
             <div key={post.id} className="posts__desc">
                 <strong>{post.title}</strong>
               <div>{post.body}</div>
