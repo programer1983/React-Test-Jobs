13ec45b187732c29ee291faaf471715897366124
programer1983 <programer972@gmail.com>
2022-11-26 11:30:38 +0200

    useEffect and API request

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import React from "react"
 import './App.css'
-import MyModal from "./MyModal"
-import PostForm from "./PostForm"
-import { usePosts } from "./usePostHook"
+import axios from "axios"
 
 function App() {
-  const [posts, setPosts] = React.useState([
-    {id: 1, title: "Ract", body: "React"},
-    {id: 2, title: "Redux", body: "Redux"},
-    {id: 3, title: "Next", body: "Next"},
-    {id: 4, title: "Django", body: "Django"},
-    {id: 5, title: "Ruby", body: "Ruby"},
-    {id: 6, title: "Java", body: "Java"},
-  ])
+  const [posts, setPosts] = React.useState([])
 
-  const [search, setSearch] = React.useState("")
+  async function fetchPosts(){
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    setPosts(response.data)
+  }
 
-  const searchPosts = usePosts(posts, search)
+  React.useEffect(() => {
+    fetchPosts()
+  }, [])
 
+  console.log(posts)
+  
+  
   return (
     <div className="App">
-
-      <input
-        type="text"
-        placeholder="Search"
-        value={search}
-        onChange={e => setSearch(e.target.value)}
-      />
-      
       <div className="posts__list">
-        {searchPosts.map((post, index) => (
+        {posts.map((post, index) => (
             <div key={post.id}  className="posts__desc">
-                <strong>{post.title}</strong>
+                <strong>{index + 1}. {post.title}</strong>
                 <div>{post.body}</div>
             </div>
           ))}
