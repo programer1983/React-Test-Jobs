31d187663ebb6702aab83e5200ec87d7e2cff99c
programer1983 <programer972@gmail.com>
2023-03-20 16:13:36 +0200

    update post

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    {"id":1,"first_name":"Deanne","last_name":"Tock"},
+    {"id":2,"first_name":"Clarey","last_name":"Baudoux"},
+    {"id":3,"first_name":"Lemmy","last_name":"Georgeau"},
+    {"id":4,"first_name":"Sharron","last_name":"Fantham"},
+    {"id":5,"first_name":"Baudoin","last_name":"Gogan"},
+    {"id":6,"first_name":"Hazel","last_name":"Greystoke"},
+    {"id":7,"first_name":"Etty","last_name":"Tiplady"},
+    {"id":8,"first_name":"Meriel","last_name":"Wickerson"},
+    {"id":9,"first_name":"Isabel","last_name":"Gingles"},
+    {"id":10,"first_name":"Bearnard","last_name":"Finlason"}
+]
